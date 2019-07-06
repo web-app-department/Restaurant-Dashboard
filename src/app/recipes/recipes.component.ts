d8c44b5072702e29de150cd9deb1ca2bb248bd8f
mukhulaazam <mukhulaazam@gmail.com>
2019-07-06 13:25:15 +0600

    Recipe model create completed and next is  Adding content to the recipe component

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recipe } from '../recipes/recipes.model';
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
   styleUrls: ['./recipes.component.css']
 })
 export class RecipesComponent implements OnInit {
+  recipes: Recipe[] = [];
+
 
   constructor() { }
 
